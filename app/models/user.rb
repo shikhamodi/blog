1cad2cb9667a86c2fe20401ab06b67886a9b72b5
shikha_modi <shikham.pis@gmail.com>
2015-11-24 14:54:47 +0530

    design changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :blogs
-	validates :email, confirmation: true
+	validates :email, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
