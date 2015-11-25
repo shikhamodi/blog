5d7ce0c9ecfd879d9d70dd22fb1fb756e71bb8c3
shikha_modi <shikham.pis@gmail.com>
2015-11-25 19:51:50 +0530

    design changes in comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -26,15 +26,14 @@ class CommentsController < ApplicationController
 
 	def create
 		@comment = @blog.comments.build(comment_params)
-		respond_to do |format|
-			if @comment.save
-				format.html 
-      	format.js
-   		else
-      	format.html { render :action => 'new' }
-      	format.js { render :action => 'new' }
-    	end
-		end
+		if @comment.save
+        format.html { redirect_to @comment, notice: 'comment was successfully upload.' }
+        format.json { render :show, status: :created, location: @comment }
+      else
+        format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
 	def update
